7da626077b3ad284a67100b8fd55e763c1715fc0
Crowdbotics <team@crowdbotics.com>
2020-11-13 12:45:43 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,9 +8,20 @@ from django.db import models
 
 class CustomText(models.Model):
     a1233455 = models.CharField(
-        max_length=150,
         blank=True,
+        max_length=150,
     )
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
     def __str__(self):
         return self.title
@@ -37,6 +48,38 @@ class CustomText(models.Model):
 
 class HomePage(models.Model):
     body = models.TextField()
+    hgjhgjhg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    jhgjhghjg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    hjjgjhgjgh = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    jhgjhgjgjhgfhjg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    ytrtyrytryr = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    hgfhfgfghfhg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
 
     @property
     def api(self):
